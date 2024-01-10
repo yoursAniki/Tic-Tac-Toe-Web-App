d9acd871768ced0289770ee9d5c54ce972a0dab4
Danila <devdaniellle@gmail.com>
2024-01-10 22:50:55 +0500

    added micro features + changing board size

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
-import "../scss/style.scss"
+import "../scss/style.scss";
+
+// Создание доски (Изменение размера)
+
+let board = document.querySelector(".game__board");
+let slider = document.querySelector(".game__slider");
+const sliderValue = document.querySelector(".game__board-size");
+const startButton = document.querySelector(".game__start-button");
+
+const changeSize = () => {
+  const size = slider.value;
+  sliderValue.textContent = `${size}x${size}`;
+
+}
+
+const createBoard = () => {
+  board.innerHTML = "";
+  const size = slider.value;
+
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+
+  for (let i = 0; i < size * size; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    board.appendChild(cell);
+  }
+};
+
+startButton.addEventListener('click', createBoard)
+
+slider.addEventListener('input', changeSize)
+
+// 
