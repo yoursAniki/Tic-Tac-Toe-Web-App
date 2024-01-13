995099786ff41060bd02b2f681152b1b58c36edb
Danila <devdaniellle@gmail.com>
2024-01-13 23:46:33 +0500

    fix cell font bug

--- src/js/main.js
+++ src/js/main.js
@@ -13,11 +13,36 @@ const changeSize = () => {
 
 }
 
+// Изменение размера шрифта (чтобы ячейки были одного)
+
+const changeFontSize = (size) => {
+  switch (size.toString()) {
+    case "3":
+      board.style.fontSize = '72px'
+      break;
+    case "4":
+      board.style.fontSize = '56px'
+      break;
+    case "5":
+      board.style.fontSize = '44px'
+      break;
+    case "7":
+      board.style.fontSize = '30px'
+      break;
+    case "8":
+      board.style.fontSize = '18px'
+      break;
+  }
+}
+
 const createBoard = () => {
   board.innerHTML = "";
   const size = slider.value;
 
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  
+  changeFontSize(size);
 
   for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
